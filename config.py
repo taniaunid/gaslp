f3415477a47cc3ee43eaf5b4c57a1f9dca7e1be4
taniaunid <00258847@red.unid.mx>
2018-02-22 21:44:10 -0600

    aplicaciongas

---
+++ config.py
@@ -0,0 +1,14 @@
+import web
+
+db_host = 'i943okdfa47xqzpy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
+db_name = 'gaslp'
+db_user = 'nv1ud6xcdpqyv2j2'
+db_pw = 'exyy8c4878431qq0'
+
+db = web.database(
+    dbn='mysql',
+    host=db_host,
+    db=db_name,
+    user=db_user,
+    pw=db_pw
+    )
